b2fdc18fef19273122230ed723879487ef10b5c7
Hector <hector.quinones.barrios@gmail.com>
2021-07-03 09:46:42 -0600

    Added linter

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
+import React, { Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'cross-fetch';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <script src="/__/firebase/8.7.0/firebase-app.js"></script>
-      <script src="/__/firebase/8.7.0/firebase-analytics.js"></script>
-      <script src="/__/firebase/init.js"></script>
-    </div>
-  );
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
